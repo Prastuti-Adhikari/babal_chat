d6f565cde6dc1d41053d7699598e9967df0fbd8e
Prastuti-Adhikari <adhikariprastuti50@gmail.com>
2024-06-25 20:33:45 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/example/babal_chat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.babal_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/utils.dart
@@ -0,0 +1,7 @@
+import 'package:babal_chat/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> setupFirebase() async{
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,
+  );
+}
