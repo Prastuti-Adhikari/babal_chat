f5109bc6e4d28a4a65ebd2efe3cc371f45681631
Prastuti-Adhikari <adhikariprastuti50@gmail.com>
2024-07-07 18:43:49 +0545

    updated

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -6,6 +6,8 @@ import 'package:babal_chat/models/user_profile.dart';
 import 'package:babal_chat/services/auth_service.dart';
 import 'package:babal_chat/services/database_service.dart';
 import 'package:babal_chat/services/media_service.dart';
+import 'package:babal_chat/services/storage_services.dart';
+import 'package:babal_chat/utils.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/material.dart';
@@ -28,6 +30,7 @@ class _ChatPageState extends State<ChatPage> {
   late AuthService _authService;
   late DatabaseService _databaseService;
   late MediaService _mediaService;
+  late StorageService _storageService;
 
   ChatUser? currentUser, otherUser;
   
@@ -37,6 +40,7 @@ class _ChatPageState extends State<ChatPage> {
     _authService = _getIt.get<AuthService>();
     _databaseService = _getIt.get<DatabaseService>();
     _mediaService = _getIt.get<MediaService>();
+    _storageService = _getIt.get<StorageService>();
     currentUser = ChatUser(
       id: _authService.user!.uid,
       firstName: _authService.user!.displayName,
@@ -89,6 +93,17 @@ class _ChatPageState extends State<ChatPage> {
   }
 
   Future<void> _sendMessage(ChatMessage chatMessage) async {
+    if (chatMessage.medias?.isNotEmpty ?? false) {
+      if(chatMessage.medias!.first.type == MediaType.image) {
+        Message message = Message(
+          senderID: chatMessage.user.id,
+          content: chatMessage.medias!.first.url,
+          messageType: MessageType.Image,
+          sentAt: Timestamp.fromDate(chatMessage.createdAt)
+          );
+          await _databaseService.sendChatMessage(currentUser!.id, otherUser!.id, message);
+      }
+    } else{
     Message message = Message (
       senderID: currentUser!.id,
       content: chatMessage.text,
@@ -101,13 +116,26 @@ class _ChatPageState extends State<ChatPage> {
       message,
       );
   }
+}
 
    List<ChatMessage> _generateChatMessagesList(List<Message> messages){
     List<ChatMessage> chatMessages = messages.map((m) {
+      if(m.messageType == MessageType.Image) {
+        return ChatMessage(
+          user: m.senderID == currentUser!.id ? currentUser! : otherUser!,
+          createdAt: m.sentAt!.toDate(),
+          medias: [
+            ChatMedia(url: m.content!, fileName: "", type: MediaType.image,
+            ),
+          ],
+          );
+      } else{
       return ChatMessage(
         user: m.senderID == currentUser!.id ? currentUser! : otherUser!,
         text: m.content!,
-        createdAt: m.sentAt!.toDate(),);
+        createdAt: m.sentAt!.toDate(),
+        );
+      }
     }).toList();
     chatMessages.sort((a,b) {
       return b.createdAt.compareTo(a.createdAt);
@@ -120,7 +148,21 @@ class _ChatPageState extends State<ChatPage> {
       onPressed: () async {
         File? file = await _mediaService.getImageFromGallery();
         if(file != null) {
-          
+          String chatID = generateChatID(
+            uid1: currentUser!.id,
+            uid2: otherUser!.id,
+          );
+           String? downloadURL = await _storageService.uploadImageToChat(
+            file: file, chatID: chatID
+            );
+
+            if(downloadURL != null) {
+              ChatMessage chatMessage = ChatMessage(user: currentUser!, createdAt: DateTime.now(), medias: [
+                ChatMedia(url: downloadURL, fileName: "", type: MediaType.image)
+              ]);
+
+              _sendMessage(chatMessage);
+            }
         }
       }, 
       icon:Icon(
--- lib/services/storage_services.dart
+++ lib/services/storage_services.dart
@@ -17,9 +17,7 @@ class StorageService {
       throw ArgumentError('File and bytes cannot both be null');
     }
 
-    Reference fileRef = _firebaseStorage
-      .ref('users/pfps')
-      .child('$uid.jpg');
+    Reference fileRef = _firebaseStorage.ref('users/pfps').child('$uid.jpg');
 
     UploadTask task;
     if (file != null) {
@@ -36,12 +34,21 @@ class StorageService {
     }
   }
 
-//   Future<String?> uploadImageToChat({
-//     required File file, required String chatID
-//   }) async {
-//     Reference fileRef = _firebaseStorage
-//     .ref('chats/$chatID')
-//     .child('${DateTime.now().toIso8601String()}${p.extension(file.path)}');
-//   UploadTask task = fileRef.putFile(file);
-//   }
- }
+  Future<String?> uploadImageToChat({
+    required File file,
+    required String chatID,
+  }) async {
+    Reference fileRef = _firebaseStorage
+        .ref('chats/$chatID')
+        .child('${DateTime.now().toIso8601String()}${p.extension(file.path)}');
+
+    UploadTask task = fileRef.putFile(file);
+
+    TaskSnapshot snapshot = await task;
+    if (snapshot.state == TaskState.success) {
+      return fileRef.getDownloadURL();
+    } else {
+      return null;
+    }
+  }
+}
